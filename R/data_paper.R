0868e4cf43c2611654dce385a2af85e7151aaf08
Sebastian Moreno <sebastian.moreno.araya@gmail.com>
2021-09-09 08:00:19 +0000

    version 1.0.0

---
+++ R/data_paper.R
@@ -0,0 +1,11 @@
+#' Example of the dataset used in the paper.
+#'
+#' A real dataset from spanish speakers, translated using automatic algorithms
+#'
+#' @format A data frame with 4364 rows and 3 variables:
+#' \describe{
+#'   \item{ID}{Id of the people}
+#'   \item{Concept}{Concept being described by the person ID}
+#'   \item{Property}{A property mentioned for the corresponding concept for the person ID}
+#' }
+"data_paper"
---
+++ R/data_test.R
@@ -0,0 +1,11 @@
+#' Test example dataset.
+#'
+#' A toy dataset containing the description of ten people, over 3 concept, with multiple properties.
+#'
+#' @format A data frame with 65 rows and 3 variables:
+#' \describe{
+#'   \item{ID}{Id of the people}
+#'   \item{Concept}{Concept being described by the person ID}
+#'   \item{Property}{A property mentioned for the corresponding concept for the person ID}
+#' }
+"data_test"
